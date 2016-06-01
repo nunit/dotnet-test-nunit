e1c339a7484bf5b475c62e5da4ff507809590398
Rob Prouse <rob@prouse.org>
2016-05-31 20:21:09 -0400

    Build NuGet package and save as artifact on AppVeyor

---
+++ scripts/set-version.ps1
@@ -0,0 +1,21 @@
+# Taken from https://github.com/Shazwazza/Smidge/ by Shannon Deminick
+
+param (
+	[Parameter(Mandatory=$true)]
+	[ValidatePattern("^\d+\.\d+\.(?:\d+\.\d+$|\d+$)")]
+	[string]
+	$ReleaseVersionNumber,
+	[Parameter(Mandatory=$true)]
+	[string]
+	[AllowEmptyString()]
+	$PreReleaseName
+)
+
+$PSScriptFilePath = (Get-Item $MyInvocation.MyCommand.Path).FullName
+$ScriptDir = Split-Path -Path $PSScriptFilePath -Parent
+$SolutionRoot = Split-Path -Path $ScriptDir -Parent
+
+ $ProjectJsonPath = Join-Path -Path $SolutionRoot -ChildPath "src\dotnet-test-nunit\project.json"
+ (gc -Path $ProjectJsonPath) `
+ 	-replace "(?<=`"version`":\s`")[.\w-]*(?=`",)", "$ReleaseVersionNumber$PreReleaseName" |
+ 	sc -Path $ProjectJsonPath -Encoding UTF8
