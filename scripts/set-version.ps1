16280ed9ea3d4cbd15554416d54a02e2ca031279
Rob Prouse <rob@prouse.org>
2016-06-10 15:06:50 -0400

    Switch the AppVeyor versioning back to the CI suffix

--- scripts/set-version.ps1
+++ scripts/set-version.ps1
@@ -1,14 +1,12 @@
 $ReleaseVersionNumber = $env:APPVEYOR_BUILD_VERSION
-$PreReleaseName = '-alpha-1'
+$PreReleaseName = ''
 
-If($env:APPVEYOR_REPO_BRANCH -eq $null -or -not $env:APPVEYOR_REPO_BRANCH.StartsWith("release")) {
-  If($env:APPVEYOR_PULL_REQUEST_NUMBER -ne $null) {
-    $PreReleaseName = '-PR-' + $env:APPVEYOR_PULL_REQUEST_NUMBER
-  } ElseIf($env:APPVEYOR_REPO_BRANCH -ne 'master') {
-    $PreReleaseName = '-' + $env:APPVEYOR_REPO_BRANCH
-  } Else {
-    $PreReleaseName = '-CI'
-  }
+If($env:APPVEYOR_PULL_REQUEST_NUMBER -ne $null) {
+  $PreReleaseName = '-PR-' + $env:APPVEYOR_PULL_REQUEST_NUMBER
+} ElseIf($env:APPVEYOR_REPO_BRANCH -ne 'master') {
+  $PreReleaseName = '-' + $env:APPVEYOR_REPO_BRANCH
+} Else {
+  $PreReleaseName = '-CI'
 }
 
 $PSScriptFilePath = (Get-Item $MyInvocation.MyCommand.Path).FullName
