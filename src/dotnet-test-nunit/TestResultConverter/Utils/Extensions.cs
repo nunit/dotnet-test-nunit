d49a168aed8d9c63ede37bd9b45aae517654b7a5
Bernardo Esbérard <bernardo.dsn.erp@alterdata.com.br>
2017-02-09 18:51:23 -0200

    Adds a capability to generate a result xml file with nunit v2 spec.

---
+++ src/dotnet-test-nunit/TestResultConverter/Utils/Extensions.cs
@@ -0,0 +1,58 @@
+﻿namespace NUnit.Runner.TestResultConverter.Utils
+{
+    using System.Linq;
+    using System.Xml;
+    using System.Xml.Linq;
+
+    public static class Extensions
+    {
+        public static XDocument GetXDocument(this XmlDocument document)
+        {
+            XDocument xDoc = new XDocument();
+            using (XmlWriter xmlWriter = xDoc.CreateWriter()) document.WriteTo(xmlWriter);
+            XmlDeclaration decl = document.ChildNodes.OfType<XmlDeclaration>().FirstOrDefault();
+            if (decl != null)
+            {
+                xDoc.Declaration = new XDeclaration(decl.Version, decl.Encoding, decl.Standalone);
+            }
+
+            return xDoc;
+        }
+
+        public static XElement GetXElement(this XmlNode node)
+        {
+            XDocument xDoc = new XDocument();
+            using (XmlWriter xmlWriter = xDoc.CreateWriter()) node.WriteTo(xmlWriter);
+            return xDoc.Root;
+        }
+
+        public static XmlDocument GetXmlDocument(this XDocument document)
+        {
+            using (XmlReader xmlReader = document.CreateReader())
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(xmlReader);
+                if (document.Declaration != null)
+                {
+                    XmlDeclaration dec = xmlDoc.CreateXmlDeclaration(
+                        document.Declaration.Version,
+                        document.Declaration.Encoding,
+                        document.Declaration.Standalone);
+                    xmlDoc.InsertBefore(dec, xmlDoc.FirstChild);
+                }
+
+                return xmlDoc;
+            }
+        }
+
+        public static XmlNode GetXmlNode(this XElement element)
+        {
+            using (XmlReader xmlReader = element.CreateReader())
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(xmlReader);
+                return xmlDoc;
+            }
+        }
+    }
+}
